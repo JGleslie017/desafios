0ab5aae234dff146b78afd5934d120e8e308d4df
JGleslie017 <126440192+JGleslie017@users.noreply.github.com>
2023-03-03 21:13:12 -0600

    Add files via upload
    0.1

---
+++ desafios/desafio 5/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Incio_sesion
+{
+    internal class program
+    {
+        static string nombre = "";
+        static string contrasena = "";
+
+        static void Main(string[] args)
+        {
+            Registrar();
+            IniciarSesion();
+
+            Console.ReadKey();
+
+        }
+
+
+        static void Registrar()
+        {
+            Console.WriteLine("Registro Ingrese nombre del usuario:");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Registro Ingrese contrasena del usuario:");
+            contrasena = Console.ReadLine();
+        }
+        static void IniciarSesion()
+        {
+            Console.WriteLine("Login Ingrese nombre del usuario:");
+            string usuarioIngresado = Console.ReadLine();
+            Console.WriteLine("login Ingrese contrasena del usuario:");
+            string contrasenaIngresada = Console.ReadLine();
+
+            if (nombre.ToLower().Equals(usuarioIngresado.ToLower()))
+            {
+                if (contrasena.Equals(contrasenaIngresada))
+                {
+                    Console.WriteLine("Bienvenido {0}", usuarioIngresado);
+                }
+                else
+                {
+                    Console.WriteLine("contrasena invalida");
+                }
+            }
+            else
+            {
+                Console.WriteLine("El usuario es invalido");
+            }
+        }
+    }
+}
